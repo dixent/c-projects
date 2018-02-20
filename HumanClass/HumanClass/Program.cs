50ba9f1deb8827e3f89d0cac107498265fbcfc14
Makarenko Andrei <dixent@inbox.ru>
2018-02-20 13:40:08 +0300

    Add files via upload

---
+++ HumanClass/HumanClass/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanClass
+{
+    class human
+    {
+        //Поле
+        private string name;
+        private int age;
+        private string sex;
+
+        //конструктор
+        public human()
+        {
+            Console.Write("Создайте нового человека.\nИмя: ");
+            name = Console.ReadLine();
+            Console.Write("Возраст: ");
+            Age = int.Parse(Console.ReadLine());
+            Console.Write("Пол: ");
+            sex = Console.ReadLine();
+
+            Console.WriteLine("Новый человек создан.");         
+        }
+
+        //методы
+        public void Speak()
+        {
+            Console.WriteLine("\nМеня зовут {0}, мне {1}, я {2}.", name, age, sex);
+        }
+
+        public static void SayHello()
+        {
+            Console.WriteLine("\nПривет.");
+        }
+
+        //свойства
+        public int Age
+        {
+            set
+            {
+                for (;;)
+                {
+                    if (value > 0)
+                    {
+                        age = value;
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("Неправильный ввод.\nВозраст: ");
+                        value = int.Parse(Console.ReadLine());
+                    }
+                }
+            }
+
+            get { return age; }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var human1 = new human();
+
+            Console.WriteLine("Нажмите 1, чтобы он рассказал о себе.");
+            if (Console.ReadKey().KeyChar == '1')
+            {
+                human.SayHello();
+                human1.Speak();
+            }    
+
+            Console.ReadLine();
+        }
+    }
+}
